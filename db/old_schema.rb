63d29b936c4d5daff2d499751a271bf86ebc84de
bkdir <direkburak1@gmail.com>
2017-09-18 23:47:23 -0400

    DB is deleted and re-created to include only users table

--- db/old_schema.rb
+++ db/old_schema.rb
@@ -10,16 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222011447) do
+ActiveRecord::Schema.define(version: 20170112024353) do
+
+  create_table "media", force: :cascade do |t|
+    t.string   "media_id",   null: false
+    t.string   "tweet_id",   null: false
+    t.boolean  "rt_media"
+    t.string   "media_url",  null: false
+    t.string   "media_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["media_id", "tweet_id"], name: "index_media_on_media_id_and_tweet_id", unique: true
+    t.index ["media_id"], name: "index_media_on_media_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "user_id",                     null: false
+    t.string   "tweet_id",                    null: false
+    t.string   "screen_name"
+    t.string   "name"
+    t.text     "text"
+    t.string   "rt_id"
+    t.text     "quoted_text"
+    t.boolean  "deleted",     default: false
+    t.datetime "tweeted_at"
+    t.datetime "deleted_at"
+    t.index ["deleted"], name: "index_tweets_on_deleted"
+    t.index ["tweet_id"], name: "index_tweets_on_tweet_id", unique: true
+    t.index ["user_id", "deleted"], name: "index_tweets_on_user_id_and_deleted"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                            null: false
     t.string   "email",                           null: false
     t.string   "password_digest"
     t.boolean  "admin",           default: false
-    t.datetime "last_login"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.datetime "last_login"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
