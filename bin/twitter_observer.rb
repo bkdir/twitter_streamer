b4df51a291ea5a82b29c69d43fc44617e18ff1c8
bkdir <direkburak1@gmail.com>
2017-09-18 23:30:26 -0400

    streamer is a singleton now.

--- bin/twitter_observer.rb
+++ bin/twitter_observer.rb
@@ -2,26 +2,37 @@
 
 require 'twitter'
 require 'tweet_handler'
+require 'singleton'
 
-$streamer = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = Rails.application.secrets.twitter_consumer_key
-  config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
-  config.access_token        = Rails.application.secrets.twitter_access_token
-  config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
-end
+class TwitterStreamer
+  include Singleton
 
-def observe_twitter
-  $streamer.user({stall_warnings: "1"}) do |object|
-    case object
-    when Twitter::Tweet
-      TweetHandler.on_post(object)
-    when Twitter::Streaming::DeletedTweet
-      TweetHandler.on_delete(object)
-    when Twitter::Streaming::StallWarning
-      TweetHandler.on_stall_warning(object)
+  @streamer = nil
+
+  def observe_twitter
+    get_streamer.user({stall_warnings: "1"}) do |object|
+      case object
+      when Twitter::Tweet
+        TweetHandler.on_post(object)
+      when Twitter::Streaming::DeletedTweet
+        TweetHandler.on_delete(object)
+      when Twitter::Streaming::StallWarning
+        TweetHandler.on_stall_warning(object)
+      end
     end
   end
+
+  private
+    def get_streamer
+      @streamer ||= Twitter::Streaming::Client.new do |config|
+        config.consumer_key        = Rails.application.secrets.twitter_consumer_key
+        config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
+        config.access_token        = Rails.application.secrets.twitter_access_token
+        config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+      end
+      @streamer
+    end
 end
 
 # start streaming
-observe_twitter
+TwitterStreamer.instance.observe_twitter
