32d25aba57e0b7f42bce73261a7a089a8b8f7693
bkdir <direkburak1@gmail.com>
2017-09-23 13:46:57 -0400

    Add password, e-mail and name validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
   def setup
     @user = User.new(name: "John Doe", email: "johndoe@example.com",
                     password: "test12", password_confirmation: "test12")
@@ -21,14 +20,26 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email address should be unique" do
+  test "email address should be unique and case insensitive" do
     dup_user = @user.dup
+    dup_user.email = @user.email.upcase
     @user.save
     assert_not dup_user.valid?
   end
+
+  test "name should not be too long" do
+    @user.name = "a" * 31
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
   
   test "email validation should accept valid email addresses" do
-    valid_emails = %w[ johndoe@example.com JohnDOE@example.COM TEST_U-ser@foo.bar.org name.lastname@test.jp john+doe@test.cn ]
+    valid_emails = %w[johndoe@example.com JohnDOE@example.COM TEST_U-ser@foo.bar.org 
+                      name.lastname@test.jp john+doe@test.cn]
     valid_emails.each do |email|
       @user.email = email
       assert @user.valid?, "#{email.inspect} should be valid"
@@ -36,11 +47,33 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid email addresses" do
-    invalid_addresses = %w[ user@example,com test_test.org user.name@example.email@test.com test@test+test.com ]
+    invalid_addresses = %w[user@example,com test_test.org 
+                           user.name@example.email@test.com test@test+test.com]
     invalid_addresses.each do |email|
       @user.email = email
       assert_not @user.valid?, "#{email.inspect} should be invalid"
     end
   end
+  
+  test "email addresses should be saved as lower-case" do
+    email = "TeST@exAMPLe.CoM"
+    @user.email = email
+    @user.save
+    assert_equal email.downcase, @user.reload.email
+  end
+  
+  test "password cannot be bland" do
+    @user.password = @user.password_confirmation =  " " * 6
+    assert_not @user.valid?
+  end
 
+  test "password should have a minmum length" do
+    @user.password = @user.password_confirmation =  "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "A nonblank and length >= 6 password should be valid" do
+    @user.password = @user.password_confirmation =  "a" * 6
+    assert @user.valid?
+  end
 end
